64662e1869ca9d8b585e1b714f00fea84abf74b1
Thomas Munzer <tmunzer@juniper.net>
2023-06-28 19:42:02 +0200

    remove spotlights files

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjY1Njc1/spectral.js?branch_slug=main&token=9eb46633-bed8-4d58-8bf8-c65fbd7f1771";
-export default { extends: ruleset };
