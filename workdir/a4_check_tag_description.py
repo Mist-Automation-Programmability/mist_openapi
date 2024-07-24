0c7ac9e43df8d88aeba7e29acbc71f7378a5a2e2
Thomas Munzer <tmunzer@juniper.net>
2024-07-24 16:58:54 +0200

    2406.1.32

--- workdir/a4_check_tag_description.py
+++ workdir/a4_check_tag_description.py
@@ -12,6 +12,13 @@ with open(FILE_IN, "r") as f:
     data = yaml.load(f, Loader=yaml.loader.SafeLoader)
     TAGS = data.get("tags", [])
 
+COUNT = 0
+TOTAL = 0
 for tag in TAGS:
+    TOTAL += 1
     if not tag.get("description"):
         print(f"description is missing for tag {tag.get('name')}")
+        COUNT += 1
+
+print()
+print(f"MISSING: {COUNT} / {TOTAL}")
