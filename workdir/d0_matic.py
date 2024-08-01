15a22ea6850384389b09911b924bfd6f4064e08b
Thomas Munzer <tmunzer@juniper.net>
2024-08-01 14:11:51 +0200

    2408.1.0

--- workdir/d0_matic.py
+++ workdir/d0_matic.py
@@ -19,9 +19,6 @@ with open(FILTER_FILE, 'r') as filter_file:
 
 with open(SPEC_FILE_IN, "r") as f:
     DATA = yaml.load(f, Loader=yaml.loader.SafeLoader)
-    INFO = DATA.get("info")
-    PATHS = DATA.get("paths")
-    COMP = DATA.get("components")
 
 # INFO["x-codegen-settings"] = {
 #     "BrandLabel": "Juniper Networks",
@@ -43,8 +40,7 @@ with open(SPEC_FILE_IN, "r") as f:
 #     "userAgent": "SDK 2024.2.1",
 #     "userConfigurableRetries": True
 # }
-INFO["x-server-configuration"] = {
-    "default-environment": "Mist Global 01",
+DATA["info"]["x-server-configuration"] = {
     "default-server": "API Host",
     "environments": [
         {
@@ -91,27 +87,6 @@ INFO["x-server-configuration"] = {
     ]
 }
 
-
-
-
-ITEMS = {
-    "orgs": {},
-    "sites": {},
-    "msps": {},
-    "installer": {},
-    "misc": {}
-}
-
-
-TAGS = {
-    "orgs": [],
-    "sites": [],
-    "msps": [],
-    "installer": [],
-    "misc": [],
-}
-
-VERBS = ["get", "post", "put", "delete"]
 ORDER = [
     "openapi",
     "info",
@@ -121,21 +96,6 @@ ORDER = [
     "paths",
     "components",
 ]
-
-
-# def reg_path(path:str, props:dict, out:dict):
-#     path_splitted = path.split("/")
-#     out_pos = out
-#     for path_part in path_splitted:
-#         if not path_part in out_pos.get("folders"):
-#             out_pos["folders"][path_part] = {"folders": {}, "endpoints": {}, "tags": []}
-#         out_pos = out_pos["folders"][path_part]
-#     out_pos["endpoints"][path] = props
-#     for verb in ["get", "post", "put", "delete"]:
-#         if verb in props:
-#             for tag in out_pos[verb]["tags"]:
-#                 if not tag in out_pos["tags"]:
-#                     out_pos["tags"].append(tag)
 
 def clean_out_folder():
     full_path = os.path.join(os.path.curdir, SPEC_FOLDER_OUT)
@@ -157,71 +117,7 @@ def check_folder(folder_path):
 #####################################################
 clean_out_folder()
 
-for path, properties in PATHS.items():
-    path_cat = path.replace("/api/v1/", "").split("/")[0]
-    if not path_cat in ITEMS:
-        path_cat = "misc"
-    
-    ITEMS[path_cat][path] = properties
-    for verb in VERBS:
-        if verb in properties:
-            for tag in properties[verb]["tags"]:
-                if not tag in TAGS[path_cat]:
-                    TAGS[path_cat].append(tag)
-
-
-API_REG = r"\"#\/components\/(?P<t>[^\/]*)\/(?P<n>[^\"]*)\""
-COMP_REG = r"#\/components\/(?P<t>[^\/]*)\/(?P<n>[^\']*)"
-
-api_folder = f"api"
-check_folder(api_folder)
-for cat, operations in ITEMS.items():
-    print(f"{cat}".ljust(15), end="")
-    if FILTER and cat not in FILTER:
-        print("not enabled")
-    else:
-        print("enabled")
-        part_folder = f"{api_folder}/{cat}"
-        check_folder(part_folder)
-        file = f"{SPEC_FOLDER_OUT}/{part_folder}/mist.openapi.{cat.lower()}.yml"
-        components = {"securitySchemes": DATA["components"]["securitySchemes"]}
-        with open(file, "w")  as oas_out_file:
-            for item in ORDER:
-                if item == "paths":
-                    yaml.dump({item: operations}, oas_out_file)
-                    operations_string = json.dumps(operations)
-                    refs = re.findall(API_REG, operations_string)
-                    for ref in refs:
-                        rtype = ref[0]
-                        rname = ref[1]
-                        if not rtype in components:
-                            components[rtype] = {}
-                        if not rname in components[rtype]:
-                            components[rtype][rname] = {"$ref": f"../../components/{rtype}/{rname}.yml"}
-                elif item == "components":
-                    yaml.dump({item: components}, oas_out_file)
-                elif item == "tags":
-                    TAGS_OUT = []
-                    for t in DATA["tags"]:
-                        if t.get("name") in TAGS[cat]:
-                            TAGS_OUT.append(t)
-                    yaml.dump({item: sorted(TAGS_OUT, key=lambda d: d['name'])}, oas_out_file)
-                else:
-                    yaml.dump({item: DATA[item]}, oas_out_file)
-
-comp_folder = f"components"
-check_folder(comp_folder)
-for part in ["parameters", "responses", "schemas"]:
-    print(part)
-    part_folder = f"{comp_folder}/{part}"
-    check_folder(part_folder)
-
-    for file_name, file_data in COMP.get(part).items():
-        file_path=f"{SPEC_FOLDER_OUT}/{part_folder}/{file_name}.yml"
-        if not file_data.get("title"):
-            file_data["title"] = (file_name.replace("_ ", " ")).title()
-        file_data_string = yaml.dump(file_data)
-        refs = re.findall(COMP_REG, file_data_string)
-        file_data_string = re.sub(COMP_REG, "../\\1/\\2.yml", file_data_string)
-        with open(file_path, "w") as f_out:
-            f_out.write(file_data_string)
+file = f"{SPEC_FOLDER_OUT}/api/mist.openapi..yml"
+with open(file, "w")  as oas_out_file:
+    for item in ORDER:
+        yaml.dump({item: DATA[item]}, oas_out_file)
